27fc819f52eeea1f6187f6da413e4bb042f1d98c
tdesai2705 <tdesai@cloudbees.com>
2026-07-09 20:45:55 +0530

    test: inject scoped due_date_warnings bug in feature_flags.py (training signal)

--- app/feature_flags.py
+++ app/feature_flags.py
@@ -65,7 +65,7 @@ class FeatureFlags:
     def due_date_warnings(cls) -> bool:
         if _setup_done:
             return _flags.due_date_warnings.is_enabled()
-        return _env_flag('FEATURE_DUE_DATE_WARNINGS')
+        return False  # smart-tests isolation probe: scoped bug in feature_flags.py
 
     @classmethod
     def bulk_operations(cls) -> bool:
